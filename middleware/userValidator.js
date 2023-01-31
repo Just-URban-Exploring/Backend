ab92a9e0a5f3149a68c01a1b938cfca7c90e052d
Uwe Kremer <uwekremer1971@gmx.de>
2023-01-31 11:39:08 +0100

    Validator erstellt

---
+++ middleware/userValidator.js
@@ -0,0 +1,22 @@
+import {body} from 'express-validator';
+
+export const userValidator = [
+  body("profilname")
+    .trim()
+    .isLength({min: 3, max: 20})
+    .withMessage('Bitte mindestens 3 Zeichen,maximal 20.')
+    .escape(),
+  body("stadt")
+    .trim()
+    .isLength({min: 3, max: 20})
+    .withMessage('Bitte mindestens 3 Zeichen,maximal 20.')
+    .escape(),
+  body("email")
+    .trim()
+    .isEmail()
+    .normalizeEmail(),
+  body("passwort")
+    .trim()
+    .isLength({min:6})
+    .withMessage('Bitte mindestens 6 Zeichen.'),
+];
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,9 +3,11 @@ import express from 'express';
 const userRouter = express.Router();
 
 import {addNewUser} from '../controller/userController.js';
+import {userValidator} from '../middleware/userValidator.js';
+import {validatorRequest} from '../middleware/validator.js';
 
 userRouter
   .route('/registration')
-    .post(addNewUser);
+    .post(userValidator, validatorRequest, addNewUser);
 
 export default userRouter;
