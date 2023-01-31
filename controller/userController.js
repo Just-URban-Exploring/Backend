ffaa347c6a45218d531a88373c1c4ab6b2cd863b
Uwe Kremer <uwekremer1971@gmx.de>
2023-01-31 09:18:07 +0100

    kein abgleich vorhandener logingdaten wie im recordshop gelöernt nötig, da es über unique gesteuert wird

--- controller/userController.js
+++ controller/userController.js
@@ -2,16 +2,7 @@ import User from '../models/userModel.js';
 
 export const addNewUser = async (req,res) => {
   try {
-    const newUser = req.body;
-    const existsUser = await User.findOne({email: newUser.email});
-    if(existsUser) {
-      const error = new Error('Diese Email gibt es bereits');
-      error.statusCode(400);
-      throw error;
-    }
-    const {profilname, stadt, email, avatar, audio, abo, _id} = await User.create({
-      ...newUser,
-    });
+    const {profilname, stadt, email, avatar, audio, abo, _id} = await User.create(req.body);
     res.status(200).send({profilname, stadt, email, avatar, audio, abo, _id});
   } catch (error) {
     console.log(error);
