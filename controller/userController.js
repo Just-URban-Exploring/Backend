832301d107b47bd38b3a54b5b4081388a190f102
Uwe Kremer <uwekremer1971@gmx.de>
2023-01-31 13:10:21 +0100

    hashen fnktioniert

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,14 @@
 import User from '../models/userModel.js';
+import bcrypt from 'bcrypt';
 
 export const addNewUser = async (req,res) => {
   try {
-    const {profilname, stadt, email, avatar, audio, abo, _id} = await User.create(req.body);
+    const newUser = req.body;
+    const hashedPw = await bcrypt.hash(newUser.passwort, 10);
+    const {profilname, stadt, email, avatar, audio, abo, _id} = await User.create({
+      ...newUser,
+      passwort: hashedPw,
+    });
     res.status(200).send({profilname, stadt, email, avatar, audio, abo, _id});
   } catch (error) {
     console.log(error);
