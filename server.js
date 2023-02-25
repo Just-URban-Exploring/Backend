69ef34ad325a2578288142a5c27abcb6941a1a0d
Uwe Kremer <uwekremer1971@gmx.de>
2023-02-25 15:01:49 +0100

    Lösung des CORS Problem

--- server.js
+++ server.js
@@ -24,7 +24,10 @@ mongoose
 mongoose.connection.on("error", console.log);
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT || 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(morgan("dev"));
 app.options('*', cors())
 app.use("/user", userRouter);
