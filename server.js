a54547e115f374ad41b26601a7f8b0e14786415c
X-Ella-X <e.felsinger@web.de>
2023-02-25 21:43:02 +0100

    add /tourguide

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ import cors from "cors";
 import morgan from "morgan";
 import userRouter from "./routes/userRoute.js";
 import errorHandler from "./middleware/errorHandler.js";
-import locationsRouter from "./routes/locationsRoute.js";
+// import locationsRouter from "./routes/locationsRoute.js";
 // import favoriteRouter from "./routes/favoriteRoute.js";
 // import cityRouter from "./routes/cityRoute.js";
 
-const URI = process.env.MONGO || "mongodb://localhost:5555/users";
-const URO = process.env.CLIENT;
+const URI = process.env.MONGO || "mongodb://localhost:5555/tourguide";
+// const URO = process.env.CLIENT;
 const app = express();
 const PORT = 5555;
 
@@ -25,10 +25,11 @@ mongoose
 mongoose.connection.on("error", console.log);
 
 app.use(express.json());
-app.use(cors({ origin: `${URO}`, credentials: true }));
+// app.use(cors({ origin: `${URO}`, credentials: true }));
+app.use(cors());
 app.use(morgan("dev"));
 app.use("/", userRouter);
-app.use("/location", locationsRouter);
+// app.use("/location", locationsRouter);
 // app.use("/city", cityRouter);
 // app.use("/favorites", favoriteRouter);
 
